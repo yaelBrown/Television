13d9891cce31b2c39d5a6d0a26097287dc88b78c
Yael Brown <yaelrbrown@gmail.com>
2017-10-01 14:54:29 -0400

    initial push
    Createed Television class for week 6 discussion. Added main method and kept everything to one file.

---
+++ Television.java
@@ -0,0 +1,77 @@
+/*
+ * File: Television.java
+ * Author: Yael Brown
+ * Date: October 1, 2017
+ * Purpose: Create a simple java class with constructor
+ * for Week 6 discussion.
+ */
+
+ /**
+  * Television Class
+  * @author Yael Brown
+  */
+public class Television {
+	// Declare variables to hold values
+	boolean isOn = false;
+	boolean working = true;
+	int currentChannel = 01;
+	
+	// Constructor
+	public Television(int chan, boolean work, boolean on) {
+		isOn = on;
+		working = work;
+		currentChannel = 01;
+	}
+	
+	// Setters
+	public void setIsOn(boolean on) {
+		on = isOn;
+	}
+	
+	public void setWorking(boolean work) {
+		working = work;
+	}
+	
+	public void setCurrentChannel(int chan) {
+		currentChannel = chan;
+	}
+	
+	// Getters
+	public boolean getIsOn() {
+		return this.isOn;
+	}
+	
+	public boolean getWorking() {
+		return this.working;
+	}
+	
+	public int getCurrentChannel() {
+		return this.currentChannel;
+	}
+	
+	// toString method
+	public String toString() {
+		String str = "/nIs TV working? " + getIsOn() + 
+				"/n Is TV working? " + getWorking() +
+				"/n The current channel is " + getCurrentChannel();
+		return str;
+	}
+	
+// Test the class
+public static void main(String[] args) {
+	// Test case 1
+	Television Zenith = new Television(12, true, true);
+	
+	// Check if the TV is on. 
+	if (Zenith.getIsOn()) {
+		System.out.println("The Zenith TV is on");
+		if (Zenith.getWorking()) {
+			System.out.println("The Zenith TV is working");
+			} else {
+				System.out.println("The Zenith TV is NOT working");
+			}
+	} else {
+			System.out.println("The Zenith TV is not on");
+			}
+		}
+}
